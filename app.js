47e98e6abecccc1ee8cf087f3a488ada04706740
Maji <start99start@gmail.com>
2023-03-05 09:07:06 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV != 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-// 引用路由器
-const routes = require('./routes')
+const routes = require('./routes') // 引用路由器
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
